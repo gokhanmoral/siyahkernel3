58e2e2989d52ed37bc62d6021ca59b7168d1594d
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-03-26 12:44:39 +0300

    support for rebooting to download mode for cm9 and clones

--- arch/arm/mach-exynos/sec-reboot.c
+++ arch/arm/mach-exynos/sec-reboot.c
@@ -91,6 +91,9 @@ static void sec_reboot(char str, const char *cmd)
 		else if (!strcmp(cmd, "download"))
 			writel(REBOOT_MODE_PREFIX | REBOOT_MODE_DOWNLOAD,
 			       S5P_INFORM3);
+		else if (!strcmp(cmd, "bootloader"))
+			writel(REBOOT_MODE_PREFIX | REBOOT_MODE_DOWNLOAD,
+			       S5P_INFORM3);
 		else if (!strcmp(cmd, "upload"))
 			writel(REBOOT_MODE_PREFIX | REBOOT_MODE_UPLOAD,
 			       S5P_INFORM3);
