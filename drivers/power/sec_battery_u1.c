5a62ae01c1f5003c2d7ee8b9380e0419ccd5f714
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-03-26 23:24:20 +0300

    currentwidget support based on Entropy512's commit in his GB kernel

--- drivers/power/sec_battery_u1.c
+++ drivers/power/sec_battery_u1.c
@@ -306,6 +306,7 @@ struct sec_bat_info {
 	unsigned int batt_temp_radc;
 #endif
 	unsigned int batt_current_adc;
+    int batt_chg_current;
 #if defined(CONFIG_TARGET_LOCALE_NAATT)
 	int batt_vf_adc;
 	int batt_event_status;
@@ -2258,6 +2259,20 @@ static void sec_bat_polling_work(struct work_struct *work)
 				      msecs_to_jiffies(info->polling_interval));
 }
 
+
+int sec_bat_check_chgcurrent(struct sec_bat_info *info)
+{
+	unsigned long cadc = 0;
+
+	mutex_lock(&info->adclock);
+	cadc = sec_bat_get_adc_data(info, ADC_CH_CHGCURRENT);
+	mutex_unlock(&info->adclock);
+	if(cadc<0) info->batt_chg_current=cadc; else
+	//fit & normalize - gm
+	info->batt_chg_current = (cadc*50-(cadc*cadc/10000*84))/100;
+	return info->batt_chg_current;
+}
+
 #define SEC_BATTERY_ATTR(_name)			\
 {						\
 	.attr = { .name = #_name,		\
@@ -2287,6 +2302,7 @@ static struct device_attribute sec_battery_attrs[] = {
 	SEC_BATTERY_ATTR(batt_test_value),
 	SEC_BATTERY_ATTR(batt_current_now),
 	SEC_BATTERY_ATTR(batt_current_adc),
+	SEC_BATTERY_ATTR(batt_chg_current),
 	SEC_BATTERY_ATTR(siop_activated),
 	SEC_BATTERY_ATTR(system_rev),
 #ifdef CONFIG_TARGET_LOCALE_NA
@@ -2341,6 +2357,7 @@ enum {
 	BATT_TEST_VALUE,
 	BATT_CURRENT_NOW,
 	BATT_CURRENT_ADC,
+	BATT_CHG_CURRENT,
 	BATT_SIOP_ACTIVATED,
 	BATT_SYSTEM_REV,
 	BATT_FG_PSOC,
@@ -2557,6 +2574,13 @@ static ssize_t sec_bat_show_property(struct device *dev,
 	case BATT_CURRENT_ADC:
 		i += scnprintf(buf + i, PAGE_SIZE - i, "%d\n",
 			       info->batt_current_adc);
+		break;
+	case BATT_CHG_CURRENT:
+		if(info->charging_status != POWER_SUPPLY_STATUS_DISCHARGING)
+		{
+			val = sec_bat_check_chgcurrent(info);
+			i += scnprintf(buf + i, PAGE_SIZE - i, "%d\n", val);
+		} else i = -EINVAL;
 		break;
 	case BATT_SYSTEM_REV:
 		i += scnprintf(buf + i, PAGE_SIZE - i, "%d\n", system_rev);
@@ -2975,6 +2999,7 @@ static int sec_bat_read_proc(char *buf, char **start,
 		      info->batt_vfocv,
 		      info->batt_vcell,
 		      info->batt_current_adc,
+              info->batt_chg_current,
 		      info->batt_full_status,
 		      info->charging_int_full_count,
 		      info->charging_adc_full_count,
