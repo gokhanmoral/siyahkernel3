2a36065e56e253507669a580109643582841f41b
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-05 00:01:39 +0300

    move inline struct to the unit where it should be (to fix compiler error with latest linaro toolchain)

--- drivers/video/samsung/s3cfb.h
+++ drivers/video/samsung/s3cfb.h
@@ -239,7 +239,7 @@ struct s3cfb_user_chroma {
 #endif
 
 extern struct fb_ops			s3cfb_ops;
-extern inline struct s3cfb_global	*get_fimd_global(int id);
+//extern inline struct s3cfb_global	*get_fimd_global(int id);
 
 /* S3CFB */
 extern struct s3c_platform_fb *to_fb_plat(struct device *dev);
