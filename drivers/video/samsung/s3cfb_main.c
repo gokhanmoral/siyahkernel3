a0ab3c1c5427bdb844affc0e8f6b6045a47eabe6
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-05 00:01:39 +0300

    move inline struct to the unit where it should be (to fix compiler error with latest linaro toolchain)

--- drivers/video/samsung/s3cfb_main.c
+++ drivers/video/samsung/s3cfb_main.c
@@ -54,7 +54,7 @@
 
 struct s3cfb_fimd_desc		*fbfimd;
 
-inline struct s3cfb_global *get_fimd_global(int id)
+/*inline struct s3cfb_global *get_fimd_global(int id)
 {
 	struct s3cfb_global *fbdev;
 
@@ -64,7 +64,7 @@ inline struct s3cfb_global *get_fimd_global(int id)
 		fbdev = fbfimd->fbdev[1];
 
 	return fbdev;
-}
+}*/
 
 int s3cfb_vsync_status_check(void)
 {
