06cedc504e9f7d9440db88c0ba96239258d81b6a
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-08 17:52:34 +0300

    build script modifications & cleanup

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,6 +2,7 @@
 export KERNELDIR=`readlink -f .`
 export INITRAMFS_SOURCE=`readlink -f $KERNELDIR/../initramfs3`
 export PARENT_DIR=`readlink -f ..`
+export USE_SEC_FIPS_MODE=true
 
 if [ "${1}" != "" ];then
   export KERNELDIR=`readlink -f ${1}`
@@ -16,42 +17,28 @@ fi
 
 . $KERNELDIR/.config
 
-rm -rf $INITRAMFS_TMP
-rm -rf $INITRAMFS_TMP.cpio
-mkdir $INITRAMFS_TMP
-
 export ARCH=arm
 export CROSS_COMPILE=$PARENT_DIR/toolchain/bin/arm-eabi-
 
 cd $KERNELDIR/
 nice -n 10 make -j4 || exit 1
 
+#remove previous initramfs files
 rm -rf $INITRAMFS_TMP
+rm -rf $INITRAMFS_TMP.cpio
+#copy initramfs files to tmp directory
 cp -ax $INITRAMFS_SOURCE $INITRAMFS_TMP
+#clear git repositories in initramfs
 find $INITRAMFS_TMP -name .git -exec rm -rf {} \;
+#remove empty directory placeholders
 find $INITRAMFS_TMP -name EMPTY_DIRECTORY -exec rm -rf {} \;
+#remove mercurial repository
 rm -rf $INITRAMFS_TMP/.hg
+#copy modules into initramfs
 mkdir -p $INITRAMFS/lib/modules
 find -name '*.ko' -exec cp -av {} $INITRAMFS_TMP/lib/modules/ \;
 
-cd $INITRAMFS_TMP
-find | fakeroot cpio -H newc -o > $INITRAMFS_TMP.cpio 2>/dev/null
-ls -lh $INITRAMFS_TMP.cpio
-cd -
-
-nice -n 10 make -j3 zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP.cpio" || exit 1
-
-cd ../payload
-rm -f ../payload.cpio
-find | fakeroot cpio -H newc -o > ../payload.cpio
-cd -
-
-cd ../recovery
-rm -f ../recovery.cpio
-rm -f ../recovery.cpio.xz
-find | fakeroot cpio -H newc -o > ../recovery.cpio
-cd -
-xz -1 ../recovery.cpio
+nice -n 10 make -j3 zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP" || exit 1
 
 cp $KERNELDIR/arch/arm/boot/zImage zImage
 #$KERNELDIR/mkshbootimg.py $KERNELDIR/zImage $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/../payload.cpio $KERNELDIR/../recovery.cpio.xz
