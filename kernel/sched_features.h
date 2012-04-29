a88765ba8b081233945e8f1462239a247f5c5cc8
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-04-29 18:07:39 +0300

    sched_debug: revert hrtick and gentle_fair_sleepers to default.

--- kernel/sched_features.h
+++ kernel/sched_features.h
@@ -3,7 +3,7 @@
  * them to run sooner, but does not allow tons of sleepers to
  * rip the spread apart.
  */
-SCHED_FEAT(GENTLE_FAIR_SLEEPERS, 0)
+SCHED_FEAT(GENTLE_FAIR_SLEEPERS, 1)
 
 /*
  * Place new tasks ahead so that they do not starve already running
@@ -49,7 +49,7 @@ SCHED_FEAT(CACHE_HOT_BUDDY, 1)
  */
 SCHED_FEAT(ARCH_POWER, 1)
 
-SCHED_FEAT(HRTICK, 1)
+SCHED_FEAT(HRTICK, 0)
 SCHED_FEAT(DOUBLE_TICK, 0)
 SCHED_FEAT(LB_BIAS, 1)
 
