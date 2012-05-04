997b2f3a2eb6b844a182718bb8518d4075950348
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-05-04 21:30:42 +0300

    cpufreq: cpufreq_conservative: tweak conservative governor settings a little more.

--- drivers/cpufreq/cpufreq_conservative.c
+++ drivers/cpufreq/cpufreq_conservative.c
@@ -29,7 +29,7 @@
  * It helps to keep variable names smaller, simpler
  */
 
-#define DEF_FREQUENCY_UP_THRESHOLD		(65)
+#define DEF_FREQUENCY_UP_THRESHOLD		(70)
 #define DEF_FREQUENCY_DOWN_THRESHOLD		(30)
 
 /*
@@ -92,7 +92,7 @@ static struct dbs_tuners {
 	.down_threshold = DEF_FREQUENCY_DOWN_THRESHOLD,
 	.sampling_down_factor = DEF_SAMPLING_DOWN_FACTOR,
 	.ignore_nice = 0,
-	.freq_step = 5,
+	.freq_step = 10,
 };
 
 static inline cputime64_t get_cpu_idle_time_jiffy(unsigned int cpu,
