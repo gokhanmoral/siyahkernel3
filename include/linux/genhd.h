786cc3b112dacf0562be29c6b08b5cc1b250b263
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-06-03 12:44:40 +0300

    scsi: merges with n7000 update3 sources

--- include/linux/genhd.h
+++ include/linux/genhd.h
@@ -128,6 +128,9 @@ struct hd_struct {
 #define GENHD_FL_EXT_DEVT			64 /* allow extended devt */
 #define GENHD_FL_NATIVE_CAPACITY		128
 #define GENHD_FL_BLOCK_EVENTS_ON_EXCL_WRITE	256
+#ifdef CONFIG_USB_HOST_NOTIFY
+#define GENHD_IF_USB	1
+#endif
 
 enum {
 	DISK_EVENT_MEDIA_CHANGE			= 1 << 0, /* media changed */
@@ -192,6 +195,7 @@ struct gendisk {
 	int node_id;
 #ifdef CONFIG_USB_HOST_NOTIFY
 	int media_present;
+	int interfaces;
 #endif
 };
 
