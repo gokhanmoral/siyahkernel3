025f0fd727a24d6da3cd57d0515196127b031d5c
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-06-12 19:50:40 +0300

    block: sio tweaks

--- block/sio-iosched.c
+++ block/sio-iosched.c
@@ -29,8 +29,8 @@ static const int sync_write_expire = 2 * HZ;	/* max time before a sync write is 
 static const int async_read_expire  =  4 * HZ;	/* ditto for async, these limits are SOFT! */
 static const int async_write_expire = 16 * HZ;	/* ditto for async, these limits are SOFT! */
 
-static const int writes_starved = 2;		/* max times reads can starve a write */
-static const int fifo_batch     = 8;		/* # of sequential requests treated as one
+static const int writes_starved = 1;		/* max times reads can starve a write */
+static const int fifo_batch     = 1;		/* # of sequential requests treated as one
 						   by the above parameters. For throughput. */
 
 /* Elevator data */
