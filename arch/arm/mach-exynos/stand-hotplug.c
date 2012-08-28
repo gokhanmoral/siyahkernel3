3e375f9339ffa56520a50e2ed5f7bf799b824399
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-08-28 13:24:17 -0700

    lulzactiveq by roberto

--- arch/arm/mach-exynos/stand-hotplug.c
+++ arch/arm/mach-exynos/stand-hotplug.c
@@ -565,6 +565,7 @@ static int standhotplug_cpufreq_policy_notifier_call(struct notifier_block *this
 	case CPUFREQ_ADJUST:
 		if (
 			(!strnicmp(policy->governor->name, "pegasusq", CPUFREQ_NAME_LEN)) ||
+			(!strnicmp(policy->governor->name, "lulzactiveq", CPUFREQ_NAME_LEN)) ||
 			(!strnicmp(policy->governor->name, "hotplug", CPUFREQ_NAME_LEN)) ||
 			(!strnicmp(policy->governor->name, "assplug", CPUFREQ_NAME_LEN))
 			) 
