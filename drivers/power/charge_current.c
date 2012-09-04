04a4a66a4ea4fb09c8c18415c1c126bd2f9e4643
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-09-04 12:48:49 -0700

    charge_current: set defauts to 650mA

--- drivers/power/charge_current.c
+++ drivers/power/charge_current.c
@@ -16,8 +16,8 @@
 #define MAX_CURRENT_MISC 950
 #define MAX_CURRENT_USB  950
 int charge_current_ac = 650;
-int charge_current_misc = 550;
-int charge_current_usb = 450;
+int charge_current_misc = 650;
+int charge_current_usb = 650;
 
 
 static ssize_t charge_current_show(struct device *dev, struct device_attribute *attr, char *buf) {
