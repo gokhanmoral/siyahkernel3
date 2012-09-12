245af372067ed94c7b7cccf33a6ba444b5c80c8e
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-09-12 16:07:16 -0700

    build script: fix strip binary location

--- build_kernel.sh
+++ build_kernel.sh
@@ -38,7 +38,7 @@ rm -rf $INITRAMFS_TMP/.hg
 mkdir -p $INITRAMFS/lib/modules
 find -name '*.ko' -exec cp -av {} $INITRAMFS_TMP/lib/modules/ \;
 chmod 644 $INITRAMFS_TMP/lib/modules/*
-${CROSS_COMPILE}strip --strip-unneeded $INITRAMFS_TMP/lib/modules/*
+/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $INITRAMFS_TMP/lib/modules/*
 
 nice -n 10 make -j3 zImage CONFIG_INITRAMFS_SOURCE="$INITRAMFS_TMP" || exit 1
 
