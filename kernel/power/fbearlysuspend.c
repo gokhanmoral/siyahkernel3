5e30bb508676fc816fa92fd8f3145e44f85e9a76
Gokhan Moral <gm@alumni.bilkent.edu.tr>
2012-10-21 16:06:53 -0700

    fbearlysuspend: make delay to play CRT animation optional

--- kernel/power/fbearlysuspend.c
+++ kernel/power/fbearlysuspend.c
@@ -28,6 +28,7 @@ static enum {
 	FB_STATE_DRAWING_OK,
 } fb_state;
 
+int fbearlysuspend_delay = 0;
 /* tell userspace to stop drawing, wait for it to stop */
 static void stop_drawing_early_suspend(struct early_suspend *h)
 {
@@ -36,7 +37,7 @@ static void stop_drawing_early_suspend(struct early_suspend *h)
 
     /* FIXME: earlysuspend breaks androids CRT-off animation
      * Sleep a little bit to get it played properly */
-    msleep(500);
+    msleep(fbearlysuspend_delay);
 
 	spin_lock_irqsave(&fb_state_lock, irq_flags);
 	fb_state = FB_STATE_REQUEST_STOP_DRAWING;
